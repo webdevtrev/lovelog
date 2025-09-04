68e6aa95715d65375e954ac5fd4c15d469e63200
webdevtrev <66264441+webdevtrev@users.noreply.github.com>
2025-09-04 15:48:22 -0400

    Initial Commit

---
+++ Lovelog/LovelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovelogApp.swift
+//  Lovelog
+//
+//  Created by Trevor Cash on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LovelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LovelogTests/LovelogTests.swift
@@ -0,0 +1,17 @@
+//
+//  LovelogTests.swift
+//  LovelogTests
+//
+//  Created by Trevor Cash on 9/4/25.
+//
+
+import Testing
+@testable import Lovelog
+
+struct LovelogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
